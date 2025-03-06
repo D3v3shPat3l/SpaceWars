d1c4bcd03c977d40ba3fa5c48157f7dfcb79a863
NotDevesh04 <151455775+NotDevesh04@users.noreply.github.com>
2025-03-06 13:57:11 +0000

    End of round points

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using TMPro;
 using System;
+using System.Collections;
+using UnityEngine.TestTools;
+using NUnit.Framework.Constraints;
 
 public class GameController : MonoBehaviour
 {
@@ -11,10 +14,16 @@ public class GameController : MonoBehaviour
     public int lasersLeft = 30;
     private int enemyUFOThisRound = 10;
     private int enemyUFOLeftInRound = 0;
+    [SerializeField] private int LaserBonusPoints = 5;
+    [SerializeField] private int PlanetsBonusPoints = 100;
 
     [SerializeField] private TextMeshProUGUI myScoreText;
     [SerializeField] private TextMeshProUGUI myLevelText;
     [SerializeField] private TextMeshProUGUI myLaserText;
+    [SerializeField] private TextMeshProUGUI LeftOverLaserBonusText;
+    [SerializeField] private TextMeshProUGUI LeftOverPlanetBonusText;
+    [SerializeField] private TextMeshProUGUI TotalBonusText;
+
 
     private int destroyUFOPoints = 25;
 
@@ -30,11 +39,10 @@ public class GameController : MonoBehaviour
 
     void Update()
     {
-        if (enemyUFOLeftInRound <= 0)
-        {
-            endOfRoundPanel.SetActive(true);
-        }
-        
+       if (enemyUFOLeftInRound <=0)
+       {
+        StartCoroutine(EndOfRound());
+       }
     }
 
     public void UpdateScoreText()
@@ -69,5 +77,23 @@ public class GameController : MonoBehaviour
         myEnemyUFOSpawner.ufoToSpawnThisRound = enemyUFOThisRound;
         enemyUFOLeftInRound = enemyUFOThisRound;
         myEnemyUFOSpawner.StartRound();
+    }
+
+    public IEnumerator EndOfRound()
+    {
+        yield return new WaitForSeconds(1f);
+        endOfRoundPanel.SetActive(true);
+        int leftOverLaserBonus = lasersLeft * LaserBonusPoints;
+
+        planets[] planets = GameObject.FindObjectsOfType<planets>();
+        int leftOverPlanetBonus = planets.Length * PlanetsBonusPoints;
+
+        int totalBonus = leftOverLaserBonus + leftOverPlanetBonus;
+
+        LeftOverLaserBonusText.text = "Left over Laser Bonus: " + leftOverLaserBonus;
+        LeftOverPlanetBonusText.text = "Left over Planet Bonus: " + leftOverPlanetBonus;
+        TotalBonusText.text = "Total Bonus: " + totalBonus;
+
+        score += totalBonus;
     }
 }
