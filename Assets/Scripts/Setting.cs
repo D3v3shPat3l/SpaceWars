454d42275f99a4e83ef3f58e1ecd8353439d3fd5
NotDevesh04 <151455775+NotDevesh04@users.noreply.github.com>
2025-03-13 14:30:44 +0000

    revert

--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -5,32 +5,15 @@ public class Setting : MonoBehaviour
 {
     [SerializeField] private Toggle musicToggle;
     [SerializeField] private Toggle soundEffectsToggle;
-    [SerializeField] private AudioSource[] soundEffectAudioSources;
 
     void Start(){
         if (musicToggle != null){
             musicToggle.isOn = !Music.instance.GetComponent<AudioSource>().mute;
             musicToggle.onValueChanged.AddListener(ToggleMusic);
         }
-
-        if (soundEffectsToggle != null){
-            soundEffectsToggle.isOn = PlayerPrefs.GetInt("SoundEffects", 1) == 1;
-            soundEffectsToggle.onValueChanged.AddListener(ToggleSoundEffects);
-        }
     }
 
     public void ToggleMusic(bool isOn){
         Music.instance.ToggleMusic(isOn);
-    }
-
-    public void ToggleSoundEffects(bool isOn){
-        PlayerPrefs.SetInt("SoundEffects", isOn ? 1 : 0);
-        PlayerPrefs.Save();
-
-        foreach (AudioSource audioSource in soundEffectAudioSources){
-            if (audioSource != null){
-                audioSource.mute = !isOn;
-            }
-        }
     }
 }
