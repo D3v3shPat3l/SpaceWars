69eab7823cc32042ff39f2f74c09603cad840797
NotDevesh04 <151455775+NotDevesh04@users.noreply.github.com>
2025-03-13 20:33:46 +0000

    Flash only the recent added player

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -1,28 +1,28 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
-using System.Collections; 
+using System.Collections;
 
-public class Leaderboard : MonoBehaviour{
+public class Leaderboard : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI[] nameTextArray;
     [SerializeField] private TextMeshProUGUI[] scoreTextArray;
     [SerializeField] private AudioClip newEntrySound;
     private AudioSource audioSource;
 
-    
-    private Color goldColor = new Color(1f, 0.843f, 0f); 
+    private Color goldColor = new Color(1f, 0.843f, 0f);
     private Color greenColor = new Color(0.494f, 0.850f, 0.341f);
 
-    void Start(){
-        audioSource = GetComponent<AudioSource>(); 
-        audioSource.playOnAwake = false;  
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
         ShowHighScores();
     }
 
-    public void ShowHighScores(){
+    public void ShowHighScores() {
         List<HighScoreEntry> highScoreEntryList = SaveScoreManager.LoadScores();
         bool newEntryAdded = false;
         bool scoreUpdated = false;
+        int flashIndex = -1;  
 
         string lastLeaderboard = PlayerPrefs.GetString("LastLeaderboard", "");
 
@@ -30,45 +30,47 @@ public class Leaderboard : MonoBehaviour{
         List<string> currentNames = new List<string>();
         List<int> currentScores = new List<int>();
 
-        if (highScoreEntryList == null || highScoreEntryList.Count == 0){
-            for (int i = 0; i < nameTextArray.Length; i++){
+        if (highScoreEntryList == null || highScoreEntryList.Count == 0) {
+            for (int i = 0; i < nameTextArray.Length; i++) {
                 nameTextArray[i].text = "---";
                 scoreTextArray[i].text = "0";
             }
             return;
         }
 
-        for (int i = 0; i < nameTextArray.Length; i++){
-            if (i < highScoreEntryList.Count){
+        for (int i = 0; i < nameTextArray.Length; i++) {
+            if (i < highScoreEntryList.Count) {
                 nameTextArray[i].text = highScoreEntryList[i].userName;
                 scoreTextArray[i].text = highScoreEntryList[i].score.ToString();
                 currentLeaderboard += highScoreEntryList[i].userName + ":" + highScoreEntryList[i].score + ",";
                 currentNames.Add(highScoreEntryList[i].userName);
                 currentScores.Add(highScoreEntryList[i].score);
 
-                if (!string.IsNullOrEmpty(lastLeaderboard)){
+                if (!string.IsNullOrEmpty(lastLeaderboard)) {
                     string[] lastEntries = lastLeaderboard.Split(',');
                     bool playerFound = false;
-                    foreach (var entry in lastEntries){
+                    foreach (var entry in lastEntries) {
                         if (string.IsNullOrEmpty(entry)) continue;
                         string[] entryParts = entry.Split(':');
                         if (entryParts.Length == 2) {
                             string lastPlayerName = entryParts[0];
-                            if (lastPlayerName == highScoreEntryList[i].userName){
+                            if (lastPlayerName == highScoreEntryList[i].userName) {
                                 playerFound = true;
                                 int previousScore = int.Parse(entryParts[1]);
-                                if (highScoreEntryList[i].score != previousScore){
+                                if (highScoreEntryList[i].score != previousScore) {
                                     scoreUpdated = true;
+                                    flashIndex = i; 
                                 }
                                 break;
                             }
                         }
                     }
-                    if (!playerFound){
-                        newEntryAdded = true; 
+                    if (!playerFound) {
+                        newEntryAdded = true;
+                        flashIndex = i; 
                     }
                 }
-            } else{
+            } else {
                 nameTextArray[i].text = "---";
                 scoreTextArray[i].text = "0";
             }
@@ -77,37 +79,34 @@ public class Leaderboard : MonoBehaviour{
         PlayerPrefs.SetString("LastLeaderboard", currentLeaderboard);
         PlayerPrefs.Save();
 
-        if ((newEntryAdded || scoreUpdated) && newEntrySound != null){
-            audioSource.playOnAwake = true; 
+        if ((newEntryAdded || scoreUpdated) && newEntrySound != null) {
             audioSource.PlayOneShot(newEntrySound);
-            audioSource.playOnAwake = false; 
-            StartCoroutine(FlashHighScoreText());
+            if (flashIndex != -1) {
+                StartCoroutine(FlashHighScoreText(flashIndex));
+            }
         }
     }
 
-    private IEnumerator FlashHighScoreText(){
-        float flashDuration = 6f; 
-        float flashInterval = 0.5f; 
+    private IEnumerator FlashHighScoreText(int index) {
+        float flashDuration = 6f;
+        float flashInterval = 0.5f;
 
         float timeElapsed = 0f;
         bool toggleColor = true;
-        while (timeElapsed < flashDuration){
+
+        TextMeshProUGUI nameText = nameTextArray[index];
+        TextMeshProUGUI scoreText = scoreTextArray[index];
+
+        while (timeElapsed < flashDuration) {
             Color targetColor = toggleColor ? goldColor : greenColor;
-            foreach (var nameText in nameTextArray){
-                nameText.color = targetColor;
-            }
-            foreach (var scoreText in scoreTextArray){
-                scoreText.color = targetColor;
-            }
-            toggleColor = !toggleColor; 
+            nameText.color = targetColor;
+            scoreText.color = targetColor;
+            toggleColor = !toggleColor;
             timeElapsed += flashInterval;
-            yield return new WaitForSeconds(flashInterval); 
+            yield return new WaitForSeconds(flashInterval);
         }
-        foreach (var nameText in nameTextArray){
-            nameText.color = greenColor;
-        }
-        foreach (var scoreText in scoreTextArray){
-            scoreText.color = greenColor;
-        }
+
+        nameText.color = greenColor;
+        scoreText.color = greenColor;
     }
 }
