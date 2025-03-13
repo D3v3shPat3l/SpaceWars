5a95f7fe0da94e500bc11438a990fca70f1b8a8b
NotDevesh04 <151455775+NotDevesh04@users.noreply.github.com>
2025-03-13 14:20:52 +0000

    Fully Functioning Settings Panel

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class Music : MonoBehaviour{
     public static Music instance;
+    private AudioSource audioSource;
 
     private void Awake(){
         if (instance == null){
             instance = this;
             DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
         } else{
             Destroy(gameObject);
+        }
+    }
+
+        public void ToggleMusic(bool isOn){
+        if (audioSource != null){
+            audioSource.mute = !isOn; 
         }
     }
 }
--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -1,13 +1,36 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Setting : MonoBehaviour{
-   
+public class Setting : MonoBehaviour
+{
+    [SerializeField] private Toggle musicToggle;
+    [SerializeField] private Toggle soundEffectsToggle;
+    [SerializeField] private AudioSource[] soundEffectAudioSources;
+
     void Start(){
-        
+        if (musicToggle != null){
+            musicToggle.isOn = !Music.instance.GetComponent<AudioSource>().mute;
+            musicToggle.onValueChanged.AddListener(ToggleMusic);
+        }
+
+        if (soundEffectsToggle != null){
+            soundEffectsToggle.isOn = PlayerPrefs.GetInt("SoundEffects", 1) == 1;
+            soundEffectsToggle.onValueChanged.AddListener(ToggleSoundEffects);
+        }
     }
 
-    public void Back(){
-        SceneManager.LoadScene("MenuScene");
+    public void ToggleMusic(bool isOn){
+        Music.instance.ToggleMusic(isOn);
+    }
+
+    public void ToggleSoundEffects(bool isOn){
+        PlayerPrefs.SetInt("SoundEffects", isOn ? 1 : 0);
+        PlayerPrefs.Save();
+
+        foreach (AudioSource audioSource in soundEffectAudioSources){
+            if (audioSource != null){
+                audioSource.mute = !isOn;
+            }
+        }
     }
 }
