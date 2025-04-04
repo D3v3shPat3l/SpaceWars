2fcb64bd8fc1e638c221f286d9db3ea2e37050d4
NotDevesh04 <151455775+NotDevesh04@users.noreply.github.com>
2025-04-04 17:33:44 +0100

    Final Submission Files

--- Assets/Scripts/GunFollowMouse.cs
+++ Assets/Scripts/GunFollowMouse.cs
@@ -10,15 +10,10 @@ public class GunFollowMouse : MonoBehaviour{
     void Update(){
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0f;
-
         Vector3 direction = mousePos - pivotPoint.position;
-
         float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
         targetAngle = Mathf.Clamp(targetAngle, minRotation, maxRotation);
-
         float smoothAngle = Mathf.LerpAngle(pivotPoint.eulerAngles.z, targetAngle, rotationSpeed * Time.deltaTime);
-
         pivotPoint.rotation = Quaternion.Euler(0, 0, smoothAngle);
     }
 }
--- Assets/Scripts/OnlineLeaderboard.cs
+++ Assets/Scripts/OnlineLeaderboard.cs
@@ -3,60 +3,47 @@ using TMPro;
 using System.Collections.Generic;
 using System.Linq;
 
-public class OnlineLeaderboard : MonoBehaviour
-{
+public class OnlineLeaderboard : MonoBehaviour{
     [SerializeField] private TextMeshProUGUI[] nameTextArray;
     [SerializeField] private TextMeshProUGUI[] scoreTextArray;
     [SerializeField] private GameObject leaderboardOnlinePanel;
     [SerializeField] private GameObject leaderboardPanel;
     private FirebaseManager firebaseManager;
 
-    void Start()
-    {
+    void Start(){
         firebaseManager = Object.FindFirstObjectByType<FirebaseManager>();
         ShowOnlineHighScores();
     }
 
-    public void ShowOnlineHighScores()
-    {
+    public void ShowOnlineHighScores(){
         StartCoroutine(firebaseManager.LoadScoresFromFirebase(OnScoresLoaded));
     }
 
-    private void OnScoresLoaded(List<OnlineHighScoreEntry> highScoreEntryList)
-    {
-        if (highScoreEntryList == null || highScoreEntryList.Count == 0)
-        {
-            for (int i = 0; i < nameTextArray.Length; i++)
-            {
+    private void OnScoresLoaded(List<OnlineHighScoreEntry> highScoreEntryList){
+        if (highScoreEntryList == null || highScoreEntryList.Count == 0){
+            for (int i = 0; i < nameTextArray.Length; i++){
                 nameTextArray[i].text = "---";
                 scoreTextArray[i].text = "0";
             }
             return;
         }
-
-        // Sort and show top entries
         var sortedList = highScoreEntryList
             .OrderByDescending(entry => entry.score)
             .Take(nameTextArray.Length)
             .ToList();
 
-        for (int i = 0; i < nameTextArray.Length; i++)
-        {
-            if (i < sortedList.Count)
-            {
+        for (int i = 0; i < nameTextArray.Length; i++){
+            if (i < sortedList.Count){
                 nameTextArray[i].text = sortedList[i].userName;
                 scoreTextArray[i].text = sortedList[i].score.ToString();
-            }
-            else
-            {
+            }else{
                 nameTextArray[i].text = "---";
                 scoreTextArray[i].text = "0";
             }
         }
     }
 
-    public void SwitchLeaderboard()
-    {
+    public void SwitchLeaderboard(){
         leaderboardOnlinePanel.SetActive(false);
         leaderboardPanel.SetActive(true);
     }
