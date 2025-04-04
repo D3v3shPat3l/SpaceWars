f224433eacea10980d134a9c9d19e04faa04b32b
NotDevesh04 <151455775+NotDevesh04@users.noreply.github.com>
2025-04-04 11:10:05 +0100

    Fixining offline bug to online

--- Assets/Scripts/OnlineLeaderboard.cs
+++ Assets/Scripts/OnlineLeaderboard.cs
@@ -1,48 +1,62 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
-public class OnlineLeaderboard : MonoBehaviour{
+using System.Linq;
+
+public class OnlineLeaderboard : MonoBehaviour
+{
     [SerializeField] private TextMeshProUGUI[] nameTextArray;
     [SerializeField] private TextMeshProUGUI[] scoreTextArray;
     [SerializeField] private GameObject leaderboardOnlinePanel;
     [SerializeField] private GameObject leaderboardPanel;
     private FirebaseManager firebaseManager;
 
-    void Start(){
+    void Start()
+    {
         firebaseManager = Object.FindFirstObjectByType<FirebaseManager>();
-        ShowOnlineHighScores(); 
+        ShowOnlineHighScores();
     }
 
-    public void ShowOnlineHighScores(){
+    public void ShowOnlineHighScores()
+    {
         StartCoroutine(firebaseManager.LoadScoresFromFirebase(OnScoresLoaded));
     }
 
-    private void OnScoresLoaded(List<OnlineHighScoreEntry> highScoreEntryList){
-        if (highScoreEntryList == null || highScoreEntryList.Count == 0){
-            for (int i = 0; i < nameTextArray.Length; i++){
+    private void OnScoresLoaded(List<OnlineHighScoreEntry> highScoreEntryList)
+    {
+        if (highScoreEntryList == null || highScoreEntryList.Count == 0)
+        {
+            for (int i = 0; i < nameTextArray.Length; i++)
+            {
                 nameTextArray[i].text = "---";
                 scoreTextArray[i].text = "0";
             }
             return;
         }
 
-        string currentLeaderboard = "";
+        // Sort and show top entries
+        var sortedList = highScoreEntryList
+            .OrderByDescending(entry => entry.score)
+            .Take(nameTextArray.Length)
+            .ToList();
 
-        for (int i = 0; i < nameTextArray.Length; i++){
-            if (i < highScoreEntryList.Count){
-                nameTextArray[i].text = highScoreEntryList[i].userName;
-                scoreTextArray[i].text = highScoreEntryList[i].score.ToString();
-                currentLeaderboard += highScoreEntryList[i].userName + ":" + highScoreEntryList[i].score + ",";
-            }else{
+        for (int i = 0; i < nameTextArray.Length; i++)
+        {
+            if (i < sortedList.Count)
+            {
+                nameTextArray[i].text = sortedList[i].userName;
+                scoreTextArray[i].text = sortedList[i].score.ToString();
+            }
+            else
+            {
                 nameTextArray[i].text = "---";
                 scoreTextArray[i].text = "0";
             }
         }
-        PlayerPrefs.SetString("LastLeaderboard", currentLeaderboard);
-        PlayerPrefs.Save();
     }
 
-    public void SwitchLeaderboard(){
+    public void SwitchLeaderboard()
+    {
         leaderboardOnlinePanel.SetActive(false);
         leaderboardPanel.SetActive(true);
     }
